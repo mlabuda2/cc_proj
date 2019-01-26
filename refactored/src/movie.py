6f1f52c7ea742f207f50d39d63a8ad32de410791
mlabuda2 <labuda1996@wp.pl>
2019-01-26 09:46:31 +0100

    Ref 4 -> Move Method

--- refactored/src/movie.py
+++ refactored/src/movie.py
@@ -19,3 +19,16 @@ class Movie(object):
     def get_title(self):
         return self._title
 
+    def get_charge(self, days_rented):
+        charge = 0
+        if self.get_price_code() == Movie.REGULAR:
+            charge += 2
+            if days_rented > 2:
+                charge += (days_rented - 2) * 1.5
+        elif self.get_price_code() == Movie.NEW_RELEASE:
+            charge += days_rented * 3
+        elif self.get_price_code() == Movie.CHILDRENS:
+            charge += 1.5
+            if days_rented > 3:
+                charge += (days_rented - 3) * 1.5
+        return charge
--- refactored/src/rental.py
+++ refactored/src/rental.py
@@ -13,18 +13,7 @@ class Rental(object):
         return self._movie
 
     def get_charge(self):
-        amount = 0
-        if self.get_movie().get_price_code() == Movie.REGULAR:
-            amount += 2
-            if self.get_days_rented() > 2:
-                amount += (self.get_days_rented() - 2) * 1.5
-        elif self.get_movie().get_price_code() == Movie.NEW_RELEASE:
-            amount += self.get_days_rented() * 3
-        elif self.get_movie().get_price_code() == Movie.CHILDRENS:
-            amount += 1.5
-            if self.get_days_rented() > 3:
-                amount += (self.get_days_rented() - 3) * 1.5
-        return amount
+        return self.get_movie().get_charge(self.get_days_rented())
 
     def get_frequent_renter_points(self):
         frequent_renter_points = 1
