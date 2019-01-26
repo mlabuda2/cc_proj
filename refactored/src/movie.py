e6ef8665efd27234c01a305cae56efb88af4c793
mlabuda2 <labuda1996@wp.pl>
2019-01-26 10:42:28 +0100

    Ref 6 -> Move Method to NewRelasePrice

--- refactored/src/movie.py
+++ refactored/src/movie.py
@@ -8,24 +8,28 @@ class Movie(object):
 
     def __init__(self, title, price_code):
         self._title = title
-        self.set_price_code(price_code)
+        self.set_price(price_code)
 
-    def set_price_code(self, arg):
+    def set_price(self, arg):
         self._price = {Movie.REGULAR: RegularPrice,
          Movie.CHILDRENS: ChildrensPrice,
          Movie.NEW_RELEASE: NewReleasePrice}[arg]()
-    
+
     def get_title(self):
         return self._title
 
     def get_charge(self, days_rented):
         return self._price.get_charge(days_rented)
 
+    def get_frequent_renter_points(self, days_rented):
+        return self._price.get_frequent_renter_points(days_rented)
+
 
 class Price(object):
 
-    def get_price_code(self):
-        raise Exception('Price is an abstract class')
+    def get_frequent_renter_points(self, days_rented):
+        return 1
+
     
 class RegularPrice(Price):
     
@@ -46,6 +50,9 @@ class NewReleasePrice(Price):
 
     def get_charge(self, days_rented):
         return days_rented * 3
+
+    def get_frequent_renter_points(self, days_rented):
+        return 2 if days_rented > 1 else 1
 
 
 class ChildrensPrice(Price):
--- refactored/src/rental.py
+++ refactored/src/rental.py
@@ -16,10 +16,7 @@ class Rental(object):
         return self.get_movie().get_charge(self.get_days_rented())
 
     def get_frequent_renter_points(self):
-        frequent_renter_points = 1
-        if self.get_movie().get_price_code() == Movie.NEW_RELEASE and self.get_days_rented() > 1:
-            frequent_renter_points += 1
-        return frequent_renter_points
+        return self._movie.get_frequent_renter_points(self.get_days_rented())
 
 
 
