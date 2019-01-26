fb62f7098e7253dc03be47c1cb602a030f16daf4
mlabuda2 <labuda1996@wp.pl>
2019-01-26 10:39:37 +0100

    Ref 5 -> Replace Conditional with Polymorphism

--- refactored/src/movie.py
+++ refactored/src/movie.py
@@ -10,9 +10,6 @@ class Movie(object):
         self._title = title
         self.set_price_code(price_code)
 
-    def get_price_code(self):
-        return self._price.get_price_code()
-
     def set_price_code(self, arg):
         self._price = {Movie.REGULAR: RegularPrice,
          Movie.CHILDRENS: ChildrensPrice,
@@ -22,21 +19,11 @@ class Movie(object):
         return self._title
 
     def get_charge(self, days_rented):
-        charge = 0
-        if self.get_price_code() == Movie.REGULAR:
-            charge += 2
-            if days_rented > 2:
-                charge += (days_rented - 2) * 1.5
-        elif self.get_price_code() == Movie.NEW_RELEASE:
-            charge += days_rented * 3
-        elif self.get_price_code() == Movie.CHILDRENS:
-            charge += 1.5
-            if days_rented > 3:
-                charge += (days_rented - 3) * 1.5
-        return charge
+        return self._price.get_charge(days_rented)
+
 
 class Price(object):
-    
+
     def get_price_code(self):
         raise Exception('Price is an abstract class')
     
@@ -45,13 +32,29 @@ class RegularPrice(Price):
     def get_price_code(self):
         return Movie.REGULAR
 
+    def get_charge(self, days_rented):
+        charge = 2
+        if days_rented > 2:
+            charge += (days_rented - 2) * 1.5
+        return charge
+
+
 class NewReleasePrice(Price):
     
     def get_price_code(self):
         return Movie.NEW_RELEASE
+
+    def get_charge(self, days_rented):
+        return days_rented * 3
+
 
 class ChildrensPrice(Price):
 
     def get_price_code(self):
         return Movie.CHILDRENS
 
+    def get_charge(self, days_rented):
+        charge = 1.5
+        if days_rented > 3:
+            charge += (days_rented - 3) * 1.5
+        return charge
