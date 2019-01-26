7e7f0e1b872825242db5370386d1ff9c9405aa01
mlabuda2 <labuda1996@wp.pl>
2019-01-26 09:35:07 +0100

    Ref 2 -> Extract method

--- refactored/src/customer.py
+++ refactored/src/customer.py
@@ -18,21 +18,20 @@ class Customer(object):
         result = 'Rental Record for ' + self.get_name() + '\n'
         
         for rental in self._rentals:
-            # determine amounts for rental line
-            this_amount = rental.get_charge()
 
-            # add frequent renter points
-            frequent_renter_points += 1
-            # add bonus for a two day new release rental
-            if rental.get_movie().get_price_code() == Movie.NEW_RELEASE and rental.get_days_rented() > 1:
-                frequent_renter_points += 1
-
+            frequent_renter_points+= self._get_frequent_renter_points(rental)
             # show figures for this rental
-            result += '\t' + rental.get_movie().get_title() + '\t' + '{0:.1f}'.format(this_amount) + '\n'
-            total_amount += this_amount
+
+            result += '\t' + rental.get_movie().get_title() + '\t' + '{0:.1f}'.format(rental.get_charge()) + '\n'
+            total_amount += rental.get_charge()
 
         # add footer lines
         result += 'Amount owed is ' + '{0:.1f}'.format(total_amount) + '\n'
         result += 'You earned ' + str(frequent_renter_points) + ' frequent renter points'
         return result
 
+    def _get_frequent_renter_points(self, rental):
+        frequent_renter_points = 1
+        if rental.get_movie().get_price_code() == Movie.NEW_RELEASE and rental.get_days_rented() > 1:
+            frequent_renter_points += 1
+        return frequent_renter_points
